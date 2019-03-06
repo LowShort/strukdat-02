3edf6b7b98d905c700138232f41e68208ccb777b
Iqbal Alif <47982446+LowShort@users.noreply.github.com>
2019-03-06 10:43:27 +0700

    Update pretest-02.cpp

--- pretest-02.cpp
+++ pretest-02.cpp
@@ -20,7 +20,7 @@ void moveZeroToFront(int arr[], int n) {
 	for (int i = 0; i < n - 1; i++)
 		for (int j = 0; j < n - i - 1; j++)
 			if (arr[j] > arr[j + 1])
-				swap(&arr[j], &arr[j + 1])
+				swap(&arr[j], &arr[j + 1]);
 }
 void input(int(&arr)[100], int& n) {
 	cout << "Masukkan jumlah data : "; cin >> n;
